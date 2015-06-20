ceded46a95d01e1bfe5c0f8a02cbf2a7129d8da5
Daisy Leung <leungtszkuk@gmail.com>
2015-06-19 23:31:02 -0400

    update code

--- Daisy_script/Plot_CD_VLA.py
+++ Daisy_script/Plot_CD_VLA.py
@@ -7,9 +7,7 @@ plots CD, and VLA,
 
 Notes:
 ------
-Manual changing sigma for different fits
 
-can be data of 0 km/s corresponds to z=2.685, 2.688, and 2.688 (velocity offset)
 '''
 
 from astropy import log
@@ -21,21 +19,8 @@ from FgContIm import *
 
 path = '../PriorImage/'
 Plotpath = '../OverlayFig/'
-z1 = '2.685_NReg.'
-zN = '2.688_NReg.'
-z = '2.685_S.'
-z2 = '2.688_S.'
-arcP = '2.685_arc.'
-arcS = '2.688_arc.'         # only bin2
 
-sym = arcP
-bin = 'bin5'
-
-print "Change %s and %s if needed \n" %(sym, bin)
-
-CD = 'CD_USB_'+sym+bin
-
-label = dict.fromkeys(['VLA', CD])
+label = dict.fromkeys(['VLA', 'mom0', 'cont'])
 for k in label.iterkeys():
     files = glob.glob(path+'*'+k+'*.fits')
     label[k] = files
@@ -57,44 +42,8 @@ vla_max = 1.08e58-4
 ra_center = VLA_ra
 dec_center = VLA_dec
 
-# 2.685 NReg:
-if sym == z1:
-    if bin == 'bin5':
-        sigma_CD = 6.34e-1
-    elif bin =='bin2':
-        sigma_CD = 3.5e-1
-elif sym == zN:
-# 2.688 NReg:
-    if bin == 'bin5':
-        sigma_CD = 6.71e-1
-    elif bin =='bin2':
-        sigma_CD = 5.138e-1
-elif sym == z:
-# 2.685 (S):
-    if bin == 'bin5':
-        sigma_CD = 7.8e-1
-    elif bin =='bin2':
-        sigma_CD = 7.65e-1
-elif sym == z2:
-# 2.688 (S):
-    if bin == 'bin5':
-        sigma_CD = 9.14e-1
-    elif bin =='bin2':
-        sigma_CD = 6.3e-1
-elif sym == arcP:
-# 2.685 (Arc channels)
-    if bin == 'bin5':
-        sigma_CD = 5.156e-1
-    if bin == 'bin2':
-        sigma_CD = 5.313e-1
-elif sym == arcS:
-# 2.688 (Arc channels)
-    if bin == 'bin2':
-        sigma_CD = 4.405e-1
-    else:
-        print "Error, %s has bin 2 only." %sym
-
-print sigma_CD
+sigma_line = 0.5161
+sigma_cont = 2.444e-4
 
 sizep = 0.0035
 sizePLine = 0.0040
@@ -102,8 +51,9 @@ sizePLine = 0.0040
 # # positions for crosses, centering on radio core
 ra_cross, dec_cross = ra_center, dec_center
 
-
 figCD = plt.figure(1, figsize=(12, 5))
+fig = plt.figure(4, figsize=(5, 5))
+
 row_a = 0.10
 width = 0.35
 x_gap = 0.05
@@ -118,9 +68,13 @@ fvlaCD = aplpy.FITSFigure(label['VLA'][0], \
         figure=figCD, subplot=[x0,row_a,width,dy])
 fvlaCD.show_grayscale() #stretch='log', vmin=vla_min, vmax=vla_max, vmid=vla_min-0.1)
 
-flinCD = aplpy.FITSFigure(label[CD][0], \
+flinCD = aplpy.FITSFigure(label['mom0'][0], \
         figure=figCD, subplot=[x0+width+2*x_gap, row_a, width, dy])
 flinCD.show_colorscale(cmap=mpl.cm.jet)
+
+figCont = aplpy.FITSFigure(label['cont'][0], \
+        figure=fig)
+figCont.show_colorscale(cmap=mpl.cm.jet)
 
 
 ########################################
@@ -128,16 +82,17 @@ flinCD.show_colorscale(cmap=mpl.cm.jet)
 ########################################
 
 fvlaCD.show_contour(label['VLA'][0], colors="lime", levels=sigma_contour_array(sigma_VLA), linewidths=2, layer='fg')
-fvlaCD.show_contour(label[CD][0], colors='red', levels=sigma_contour_CARMA(sigma_CD), linewidths=2, layer='fg_cont')
-flinCD.show_contour(label[CD][0], colors="white", levels=sigma_contour_CARMA(sigma_CD), linewidths=2, layer='mol')
+fvlaCD.show_contour(label['mom0'][0], colors='red', levels=sigma_contour_CARMA(sigma_line), linewidths=2, layer='fg_cont')
+flinCD.show_contour(label['mom0'][0], colors="white", levels=sigma_contour_CARMA(sigma_line), linewidths=2, layer='mol')
 
-
+figCont.show_contour(label['cont'][0], colors='red', levels=sigma_contour_CARMA(sigma_cont), linewidths=1.5, layer='fg_cont')
 ########################################
 # beam
 ########################################
 
 setup_beam(fvlaCD)
 setup_beam(flinCD)
+setup_beam(figCont)
 
 
 ########################################
@@ -156,10 +111,10 @@ lg_1arcsec = 1./3600
 ########################################
 standard_plot_setup(fvlaCD, ra_center, dec_center, sizep)
 standard_plot_setup(flinCD, ra_center, dec_center, sizePLine)
+standard_plot_setup(figCont, ra_center, dec_center, sizePLine)
 # fcont.tick_labels.hide()
 # fcont.axis_labels.hide()
-# flin.tick_labels.hide()
-# flin.axis_labels.hide()
+flinCD.axis_labels.hide()
 
 
 ########################################
@@ -167,14 +122,16 @@ standard_plot_setup(flinCD, ra_center, dec_center, sizePLine)
 ########################################
 markers_cross(fvlaCD, ra_cross, dec_cross, layer='marker_set_1')
 markers_cross(flinCD, ra_cross, dec_cross, layer='marker_set_1')
+markers_cross(figCont, ra_cross, dec_cross, layer='marker_set_1')
 
 ########################################
 # Labels
 ########################################
 put_label(fvlaCD, 0.25, 0.95, 'VLA 6GHz', 'titleBand')
 put_label(fvlaCD, 0.2625, 0.9, 'NA.v1.489', 'titleObj')
-put_label(flinCD, 0.5, 0.95, 'CARMA CO(3-2) CD data'+sym[:-1], 'titleBand', c='black')
+put_label(flinCD, 0.5, 0.95, 'CARMA CO(3-2)', 'titleBand', c='black')
 put_label(flinCD, 0.31, 0.9, 'NA.v1.489', 'titleObj', c='black')
+put_label(figCont, 0.5, 0.95, 'CARMA Continuum', 'titleBand', c='black')
 
 labsize = 'xx-large'
 labc = 'white'
@@ -204,9 +161,10 @@ if __name__ == '__main__':
         raise IndexError(errmsg.format(len(sys.argv)))
     saveFig = True if sys.argv[1].lower() == 'true' else False
     if saveFig == True:
-        os.system('rm -rf ' + CD + '.png' + ' ' + CD + '.eps')
+        os.system('rm -rf ' + 'CO32mom0.png' + ' ' + 'CO32mom0.eps')
 #        figCD.savefig(Plotpath + CD + '.eps', dpi=600)
-        print ("saving to {:s}").format(Plotpath + CD + '.png')
-        figCD.savefig(Plotpath + CD + '.png', dpi=300)
+        print ("saving to {:s}").format(Plotpath + 'CO32mom0.png')
+        figCD.savefig(Plotpath + 'CO32mom0.png', dpi=300)
+        figCont.savefig(Plotpath + 'Cont.png', dpi=300)
     else:
         plt.show()
